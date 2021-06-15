2cb51120b669b7422034031f7e90a2f57531b013
FaQ <FaQ@PC_RAMOS>
2021-06-15 17:04:53 -0300

    Entidad ProductLine

--- STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/ProductLine.java
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/ProductLine.java
@@ -1,5 +1,55 @@
 package ar.edu.unju.fi.tpfinal.model;
 
+import java.util.Arrays;
+
 public class ProductLine {
+	
+	private	char[] Linea;
+	private char[] descripcionTexto;
+	private String descripcionHtml;
+	private byte[] imagen;
+	
+	public ProductLine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public char[] getLinea() {
+		return Linea;
+	}
+
+	public void setLinea(char[] linea) {
+		Linea = linea;
+	}
+
+	public char[] getDescripcionTexto() {
+		return descripcionTexto;
+	}
+
+	public void setDescripcionTexto(char[] descripcionTexto) {
+		this.descripcionTexto = descripcionTexto;
+	}
+
+	public String getDescripcionHtml() {
+		return descripcionHtml;
+	}
+
+	public void setDescripcionHtml(String descripcionHtml) {
+		this.descripcionHtml = descripcionHtml;
+	}
+
+	public byte[] getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(byte[] imagen) {
+		this.imagen = imagen;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductLine [Linea=" + Arrays.toString(Linea) + ", descripcionTexto="
+				+ Arrays.toString(descripcionTexto) + ", descripcionHtml=" + descripcionHtml + ", imagen="
+				+ Arrays.toString(imagen) + "]";
+	}
 	
 }
