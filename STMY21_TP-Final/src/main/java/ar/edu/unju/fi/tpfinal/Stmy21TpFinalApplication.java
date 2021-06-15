01062eef01a16be4f1e7a3cd0f207ce132d4b28d
FaQ <FaQ@PC_RAMOS>
2021-06-15 16:53:06 -0300

    Primer Commit, a partir de aqui dividir en ramas segun las respectivas tareas.

---
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/Stmy21TpFinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stmy21TpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stmy21TpFinalApplication.class, args);
+	}
+
+}
---
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/ProductLine.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.tpfinal.model;
+
+public class ProductLine {
+	
+}
