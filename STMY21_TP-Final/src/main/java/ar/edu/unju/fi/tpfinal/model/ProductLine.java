413fe5c2b06242d75da38b95b4cae2af19fa4e91
FaQ <FaQ@PC_RAMOS>
2021-06-15 23:28:07 -0300

    Entidad ProductoLine redefinida

--- STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/ProductLine.java
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/ProductLine.java
@@ -2,54 +2,73 @@ package ar.edu.unju.fi.tpfinal.model;
 
 import java.util.Arrays;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table (name = "PRODUCTLINES")
+@Component("productLineObj")
 public class ProductLine {
 	
-	private	char[] Linea;
-	private char[] descripcionTexto;
-	private String descripcionHtml;
-	private byte[] imagen;
+	@Id
+	@NotNull(message = "Este campo no puede estar vacio")
+	@Column (name = "productLine")
+	private	String productLine;
+	
+	@Column (name = "textDescription")
+	private String textDescription;
+	
+	@Column (name = "htmlDescription")
+	private String htmlDescription;
+	
+	@Column (name = "image")
+	private byte[] image;
 	
 	public ProductLine() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public char[] getLinea() {
-		return Linea;
+	public String getProductLine() {
+		return productLine;
 	}
 
-	public void setLinea(char[] linea) {
-		Linea = linea;
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
 	}
 
-	public char[] getDescripcionTexto() {
-		return descripcionTexto;
+	public String getTextDescription() {
+		return textDescription;
 	}
 
-	public void setDescripcionTexto(char[] descripcionTexto) {
-		this.descripcionTexto = descripcionTexto;
+	public void setTextDescription(String textDescription) {
+		this.textDescription = textDescription;
 	}
 
-	public String getDescripcionHtml() {
-		return descripcionHtml;
+	public String getHtmlDescription() {
+		return htmlDescription;
 	}
 
-	public void setDescripcionHtml(String descripcionHtml) {
-		this.descripcionHtml = descripcionHtml;
+	public void setHtmlDescription(String htmlDescription) {
+		this.htmlDescription = htmlDescription;
 	}
 
-	public byte[] getImagen() {
-		return imagen;
+	public byte[] getImage() {
+		return image;
 	}
 
-	public void setImagen(byte[] imagen) {
-		this.imagen = imagen;
+	public void setImage(byte[] image) {
+		this.image = image;
 	}
 
 	@Override
 	public String toString() {
-		return "ProductLine [Linea=" + Arrays.toString(Linea) + ", descripcionTexto="
-				+ Arrays.toString(descripcionTexto) + ", descripcionHtml=" + descripcionHtml + ", imagen="
-				+ Arrays.toString(imagen) + "]";
+		return "ProductLine [productLine=" + productLine + ", textDescription=" + textDescription + ", htmlDescription="
+				+ htmlDescription + ", image=" + Arrays.toString(image) + "]";
 	}
-	
+
 }
