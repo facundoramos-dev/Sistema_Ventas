b1aeada06e68925af128d4473539a6c23052505b
FaQ <FaQ@PC_RAMOS>
2021-06-15 23:55:48 -0300

    Entidad Product redefinida.

--- STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/Product.java
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/Product.java
@@ -1,101 +1,137 @@
 package ar.edu.unju.fi.tpfinal.model;
 
-import java.util.Arrays;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table (name = "PRODUCTS")
+@Component("productObj")
 public class Product {
 
-	private	char[] codProducto;
-	private char[] nomProducto;
-	private ProductLine linProducto;
-	private char[] escProducto;
-	private char[] vendProducto;
-	private char[] descProducto;
-	private short cantStock;
-	private double precioCompra;
-	private double msrp;
+	@Id
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "productCode")
+	private	String productCode;
+	
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "productName")
+	private String productName;
+	
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "productLine")
+	private ProductLine productLine;
+	
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "productScale")
+	private String productScale;
+
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "productVendor")
+	private String productVendor;
+
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "productDescription")
+	private String productDescription;
+
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "quantityInStock")
+	private short quantityInStock;
+
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "buyPrice")
+	private double buyPrice;
+
+	@NotNull(message = "Este campo fecha no puede estar vacio")
+	@Column (name = "MSRP")
+	private double MSRP;
 	
 	public Product() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public char[] getCodProducto() {
-		return codProducto;
+	public String getProductCode() {
+		return productCode;
 	}
 
-	public void setCodProducto(char[] codProducto) {
-		this.codProducto = codProducto;
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
 	}
 
-	public char[] getNomProducto() {
-		return nomProducto;
+	public String getProductName() {
+		return productName;
 	}
 
-	public void setNomProducto(char[] nomProducto) {
-		this.nomProducto = nomProducto;
+	public void setProductName(String productName) {
+		this.productName = productName;
 	}
 
-	public ProductLine getLinProducto() {
-		return linProducto;
+	public ProductLine getProductLine() {
+		return productLine;
 	}
 
-	public void setLinProducto(ProductLine linProducto) {
-		this.linProducto = linProducto;
+	public void setProductLine(ProductLine productLine) {
+		this.productLine = productLine;
 	}
 
-	public char[] getEscProducto() {
-		return escProducto;
+	public String getProductScale() {
+		return productScale;
 	}
 
-	public void setEscProducto(char[] escProducto) {
-		this.escProducto = escProducto;
+	public void setProductScale(String productScale) {
+		this.productScale = productScale;
 	}
 
-	public char[] getVendProducto() {
-		return vendProducto;
+	public String getProductVendor() {
+		return productVendor;
 	}
 
-	public void setVendProducto(char[] vendProducto) {
-		this.vendProducto = vendProducto;
+	public void setProductVendor(String productVendor) {
+		this.productVendor = productVendor;
 	}
 
-	public char[] getDescProducto() {
-		return descProducto;
+	public String getProductDescription() {
+		return productDescription;
 	}
 
-	public void setDescProducto(char[] descProducto) {
-		this.descProducto = descProducto;
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
 	}
 
-	public short getCantStock() {
-		return cantStock;
+	public short getQuantityInStock() {
+		return quantityInStock;
 	}
 
-	public void setCantStock(short cantStock) {
-		this.cantStock = cantStock;
+	public void setQuantityInStock(short quantityInStock) {
+		this.quantityInStock = quantityInStock;
 	}
 
-	public double getPrecioCompra() {
-		return precioCompra;
+	public double getBuyPrice() {
+		return buyPrice;
 	}
 
-	public void setPrecioCompra(double precioCompra) {
-		this.precioCompra = precioCompra;
+	public void setBuyPrice(double buyPrice) {
+		this.buyPrice = buyPrice;
 	}
 
-	public double getMsrp() {
-		return msrp;
+	public double getMSRP() {
+		return MSRP;
 	}
 
-	public void setMsrp(double msrp) {
-		this.msrp = msrp;
+	public void setMSRP(double mSRP) {
+		MSRP = mSRP;
 	}
 
 	@Override
 	public String toString() {
-		return "Product [codProducto=" + Arrays.toString(codProducto) + ", nomProducto=" + Arrays.toString(nomProducto)
-				+ ", linProducto=" + linProducto + ", escProducto=" + Arrays.toString(escProducto) + ", vendProducto="
-				+ Arrays.toString(vendProducto) + ", descProducto=" + Arrays.toString(descProducto) + ", cantStock="
-				+ cantStock + ", precioCompra=" + precioCompra + ", msrp=" + msrp + "]";
+		return "Product [productCode=" + productCode + ", productName=" + productName + ", productLine=" + productLine
+				+ ", productScale=" + productScale + ", productVendor=" + productVendor + ", productDescription="
+				+ productDescription + ", quantityInStock=" + quantityInStock + ", buyPrice=" + buyPrice + ", MSRP="
+				+ MSRP + "]";
 	}
-
+	
 }
