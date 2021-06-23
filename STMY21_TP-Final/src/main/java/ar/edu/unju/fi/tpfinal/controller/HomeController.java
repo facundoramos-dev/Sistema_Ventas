4687929eacdf4de19dcc6e5e9c594ff8f0f5a949
FaQ <FaQ@PC_RAMOS>
2021-06-23 18:57:23 -0300

    Vistas Vista de Office

--- STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/controller/HomeController.java
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/home/")
+	@GetMapping("/home")
 	public String homeGetPage(Model model) {
 		System.out.println("Hola");
 		return "home";
