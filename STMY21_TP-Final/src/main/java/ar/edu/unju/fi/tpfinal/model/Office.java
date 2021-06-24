5c9483a09b4ceff6334487a8aec423c51f486604
LENOVO <LENOVO@LAPTOP-HKPSLP4B>
2021-06-24 20:36:58 -0300

    realizacion de archivos en html, para los repectivos formularios

--- STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/Office.java
+++ STMY21_TP-Final/src/main/java/ar/edu/unju/fi/tpfinal/model/Office.java
@@ -62,77 +62,115 @@ public class Office{
 		// TODO Auto-generated constructor stub
 	}
 
+	
+
 	public long getOfficeCode() {
 		return officeCode;
 	}
+
+
 
 	public void setOfficeCode(long officeCode) {
 		this.officeCode = officeCode;
 	}
 
+
+
 	public String getCity() {
 		return city;
 	}
+
+
 
 	public void setCity(String city) {
 		this.city = city;
 	}
 
+
+
 	public String getPhone() {
 		return phone;
 	}
+
+
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+
+
 	public String getAddressLine1() {
 		return addressLine1;
 	}
+
+
 
 	public void setAddressLine1(String addressLine1) {
 		this.addressLine1 = addressLine1;
 	}
 
+
+
 	public String getAddressLine2() {
 		return addressLine2;
 	}
+
+
 
 	public void setAddressLine2(String addressLine2) {
 		this.addressLine2 = addressLine2;
 	}
 
+
+
 	public String getState() {
 		return state;
 	}
+
+
 
 	public void setState(String state) {
 		this.state = state;
 	}
 
+
+
 	public String getCountry() {
 		return country;
 	}
+
+
 
 	public void setCountry(String country) {
 		this.country = country;
 	}
 
+
+
 	public String getPostalCode() {
 		return postalCode;
 	}
+
+
 
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
 
+
+
 	public String getTerritory() {
 		return territory;
 	}
 
+
+
 	public void setTerritory(String territory) {
 		this.territory = territory;
 	}
+
+
 
 	@Override
 	public String toString() {
